3a619d3ca25921f375fa61423b93ad8ba88eb2a5
Minh Phan <minh2442001@gmail.com>
2021-11-12 14:37:50 +0700

    Binding with properties resources

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -71,5 +71,34 @@ namespace GoninDigital.Properties {
                 return ((string)(this["DBStr"]));
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsd=\"http://www.w3." +
+            "org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">\r\n  <s" +
+            "tring>Male</string>\r\n  <string>Female</string>\r\n  <string>Other</string>\r\n</Arra" +
+            "yOfString>")]
+        public global::System.Collections.Specialized.StringCollection Gender {
+            get {
+                return ((global::System.Collections.Specialized.StringCollection)(this["Gender"]));
+            }
+            set {
+                this["Gender"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsd=\"http://www.w3." +
+            "org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">\r\n  <s" +
+            "tring>Customer</string>\r\n  <string>Seller</string>\r\n</ArrayOfString>")]
+        public global::System.Collections.Specialized.StringCollection Role {
+            get {
+                return ((global::System.Collections.Specialized.StringCollection)(this["Role"]));
+            }
+            set {
+                this["Role"] = value;
+            }
+        }
     }
 }
